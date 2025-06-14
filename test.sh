8e68f3bb7d92dd20cc8f4ab371b6842ad4558bdb
root <root@ip-10-0-1-142.ec2.internal>
2025-06-14 16:35:11 +0000

    1st modification

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+HN=$(curl http://169.254.169.254/latest/meta-data/hostname)
+PIP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+II=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+P=$(curl http://169.254.169.254/latest/meta-data/profile)
+echo "The public host name is $HN"
+echo "$PIP"
+echo "$II"
+echo "$P"
